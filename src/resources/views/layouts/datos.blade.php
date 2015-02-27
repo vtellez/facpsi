d41ffc717b09dc492769ca2cd6f7309d5b64fbd6
Víctor Téllez <tellez.victor@gmail.com>
2015-02-27 19:20:08 +0100

    fixed

--- src/resources/views/layouts/datos.blade.php
+++ src/resources/views/layouts/datos.blade.php
@@ -30,11 +30,7 @@ Datos del título
           </li>
           <li>
             <i class="icon-li fa fa-angle-right"></i>
-            <a href="{{ url('datos?s=3') }}">Códigos ISCED 1 y ISCED 2</a>
-          </li>
-          <li>
-            <i class="icon-li fa fa-angle-right"></i>
-            <a href="{{ url('datos?s=4') }}">Datos del Servicio de Doctorado</a>
+            <a href="{{ url('datos?s=4') }}">Códigos ISCED 1 y ISCED 2</a>
           </li>
           <li>
             <i class="icon-li fa fa-angle-right"></i>
@@ -70,33 +66,41 @@ Datos del título
   </div> <!-- /.col -->
   <div class="col-md-9 col-sm-8">
     @if ( $step == 1 )
-Doctorado
-Denominación del título
-Programa de Doctorado en Psicología por la Universidad de Sevilla
-    @elseif ( $step == 2 )
-    Programa de doctorado en Psicología por la Universidad de Sevilla (RD 99/2011)
+    <h2>Nivel</h2>
+    <p class="lead">&nbsp;</p>
+    <p>Doctorado</p>
+    <br><br>
+    <p class="lead">Denominación del título</p>
+    <p>Programa de Doctorado en Psicología por la Universidad de Sevilla</p>
 
-    @elseif ( $step == 3 )
-    Programa de Doctorado en Psicología por la Universidad de Sevilla
+
+    @elseif ( $step == 2 )
+    <h2>Real decreto</h2>
+    <p class="lead">&nbsp;</p>
+    <p>Programa de doctorado en Psicología por la Universidad de Sevilla (RD 99/2011)</p>
+
     @elseif ( $step == 4 )
-    ISCED 1: Psicología. ISCED 2: Psicología
+    <h2>Códigos ISCED 1 y ISCED 2</h2>
+    <p class="lead">&nbsp;</p>
+    <p>ISCED 1: Psicología</p>
+    <p>ISCED 2: Psicología</p>
     @elseif ( $step == 5 )
     Escuela Internacional de Doctorado de la Universidad de Sevilla
     @elseif ( $step == 6 )
     El PD en Psicología por la US contempla la admisión de estudiantes a tiempo completo TC y a tiempo parcial (TP). La duración de los estudios de doctorado es, por tanto:
     TC (3 años + 2 prórrogas de 1 año)
-TP (5 años + 1 prórroga de 2 años + 1 prórroga de 1 año)
+    TP (5 años + 1 prórroga de 2 años + 1 prórroga de 1 año)
     @elseif ( $step == 7 )
     www.doctorado.us.es/impresos/verificacion/NORMAS%20DE%20PERMANE NCIA_web.pdf
     @elseif ( $step == 8 )
     Castellano e inglés.
     @elseif ( $step == 9 )
     No de plazas de nuevo ingreso ofertadas por curso académico
-Curso 2013-1014: 40 plazas (16 TC y 24 TP) Curso 2014-2015: 40 plazas (16TC y 24 TP)
+    Curso 2013-1014: 40 plazas (16 TC y 24 TP) Curso 2014-2015: 40 plazas (16TC y 24 TP)
     @elseif ( $step == 10 )
     No de alumnos de nuevo ingreso matriculados por curso académico
-Curso 2013-1014: 38 estudiantes matriculados/as
-Curso 2014-2015: 27 estudiantes matriculados/as en el primer plazo. 6 estudiantes admitidos/as en el segundo plazo. Pendientes de matriculación
+    Curso 2013-1014: 38 estudiantes matriculados/as
+    Curso 2014-2015: 27 estudiantes matriculados/as en el primer plazo. 6 estudiantes admitidos/as en el segundo plazo. Pendientes de matriculación
     @else
     Incluir documento “Convenios PD”
     @endif
--- src/resources/views/layouts/presentacion.blade.php
+++ src/resources/views/layouts/presentacion.blade.php
@@ -45,16 +45,16 @@ Presentación
 
     @if ( $step == 1 )
     <h2>Antecedentes y descripción</h2>
-    <p class="lead">Breve descripción</p>
+    <p class="lead">&nbsp;</p>
     <p>Dentro de este apartado se incluye información referida al contexto de la Universidad de Sevilla (La Escuela Internacional de Doctorado de la US) y del Doctorado en Psicología.</p>
-    <p>ESCUELA INTERNACIONAL DE DOCTORADO DE LA UNIVERSIDAD DE SEVILLA (EIDUS)</p>
+    <p>ESCUELA INTERNACIONAL DE DOCTORADO DE LA UNIVERSIDAD DE SEVILLA (EIDUS): <a href="http://www.doctorado.us.es/plan-2011/escuela-internacional-de-doctorado">http://www.doctorado.us.es/plan-2011/escuela-internacional-de-doctorado</a></p>
     <br>
     <br/>
     <p class="lead">EL DOCTORADO EN PSICOLOGÍA</p>
     <p>Las universidades extranjeras, especialmente en el ámbito anglosajón, vienen ofreciendo programas de doctorado que abarcan la psicología como un todo. Son programas organizados desde Departamentos de Psicología (en el contexto anglosajón el equivalente a escuelas, institutos o facultades) que organizan doctorados integrados que abarcan distintos ámbitos tanto de la psicología aplicada, como de la investigación básica en esta disciplina. Este tipo de programas aprovechan la potencialidad que supone la convivencia e integración de las distintas ramas de la psicología que en la tradición española han venido hasta la fecha programas separados. Esta integración permite mejorar las sinergias de la cooperación y la transversalidad. En esta línea se encuentran los programas de doctorado en psicología de universidades de EE.UU. tan prestigiosas como Stanford, Michigan-Ann–Arbor, Yale, California-Los Ángeles, Illinois-Urbana- Champaign o Harvard que ocupan las 6 primeras posiciones del ranking NRC para psicología (<a href="http://www.socialpsychology.org/ranking.htm">http://www.socialpsychology.org/ranking.htm</a>).
     </p>
     <p>
-      El mismo caso se da en algunas universidades del Reino Unido. Las universidades de Birminghan, Collegue London, Birbeck-University of London, Royal Holloway- University of London, Cardiff y Warwick que ocupan los puestos 2, 4, 5, 6 y 7 del ranking NRC Británico desarrollan programas de doctorado integrados en psicología (http:// www.socialpsychology.org/ukranks.htm).
+      El mismo caso se da en algunas universidades del Reino Unido. Las universidades de Birminghan, Collegue London, Birbeck-University of London, Royal Holloway- University of London, Cardiff y Warwick que ocupan los puestos 2, 4, 5, 6 y 7 del ranking NRC Británico desarrollan programas de doctorado integrados en psicología (<a href="http:// www.socialpsychology.org/ukranks.htm">http:// www.socialpsychology.org/ukranks.htm</a>).
     </p>
     <p>Además, los programas integrados en psicología desarrollados por un único departamento que afecta a todas las ramas de especialización son la pauta general tanto en EE.UU como el Reino Unido, como puede verse en las páginas web incluidas más arriba.</p>
     <br>
@@ -63,26 +63,57 @@ Presentación
     <p>En el período 2007-2011 (último año en el que estuvo permitida la inscripción en los antiguos programas) la demanda de los distintos programas de doctorado coordinados por los diferentes Departamentos de Psicología de la Universidad de Sevilla (Departamento de Personalidad, Evaluación y Tratamiento Psicológicos, Departamento de Psicología Social, Departamento de Psicología Evolutiva y de la Educación y Departamento de Psicología Experimental) registraron una matrícula global de 248 alumnos nacionales y 33 extranjeros, con rangos que fueron entre 71 (curso 2009-10) y 46 (2011- 12), con una media de 56,2 alumnos por curso. Dado que la propuesta que ahora se presenta se puede considerar la unión de los contenidos y líneas de los cuatro programas pre-existentes, se puede deducir que la demanda de la propuesta estará en los límites del conjunto de doctorados anteriores. Una cifra de 40-50 doctores en psicología podría ser suficiente para atender las demandas de especialistas investigadores de alto nivel en el entorno social inmediato.</p>
 
     @elseif ( $step == 2 )
-
-    - Manuel L. de la Mata Benítez (TU). Vicedecano de Recursos para el Aprendizaje y la Investigación. Coordinador del Programa de Doctorado.
-    - Eva Ma Trigo Sánchez (TU). Vicedecana de Transferencia Tecnológica y Programas Internacionales.
-    - Gonzalo de la Casa Rivas (CU) Suplentes: Juan Carlos López García (TU) y Manuel Vázquez Marrufo (TU).
-    - David Saldaña Sage (TU). Carlos M. Gómez González (CU) y José León Carrión (TU).
-    - Jesús Palacios González (CU). Suplentes: Ma Carmen Moreno Rodríguez (CU) y Alfredo Oliva Delgado (TU).
-    - Joaquín Mora Merchán (TU). Suplentes: Rosario del Rey Alamillo (TU) y Virginia Sánchez Jiménez (TU).
-    - Ma Ángeles Pérez San Gregorio (CU). Suplentes: Inmaculada Moreno García (TU) y Agustín Martín Rodríguez (TU).
-    - Manuel García Ramírez (TU). Suplentes: Ana Ma López Jiménez (TU) y Andrés Santamaría Santigosa (TU).
+    <h2>Composición de la Comisión Académica</h2>
+    <p class="lead">&nbsp;</p>
+    <ul>
+      <li>
+        Manuel L. de la Mata Benítez (TU). Vicedecano de Recursos para el Aprendizaje y la Investigación. Coordinador del Programa de Doctorado.
+      </li>
+      <li>
+        Eva Ma Trigo Sánchez (TU). Vicedecana de Transferencia Tecnológica y Programas Internacionales.
+      </li>
+      <li>
+        Gonzalo de la Casa Rivas (CU) Suplentes: Juan Carlos López García (TU) y Manuel Vázquez Marrufo (TU).
+      </li>
+      <li>
+        David Saldaña Sage (TU). Carlos M. Gómez González (CU) y José León Carrión (TU).
+      </li>
+      <li>
+        Jesús Palacios González (CU). Suplentes: Ma Carmen Moreno Rodríguez (CU) y Alfredo Oliva Delgado (TU).
+      </li>
+      <li>
+        Joaquín Mora Merchán (TU). Suplentes: Rosario del Rey Alamillo (TU) y Virginia Sánchez Jiménez (TU).
+      </li>
+      <li>
+        Ma Ángeles Pérez San Gregorio (CU). Suplentes: Inmaculada Moreno García (TU) y Agustín Martín Rodríguez (TU).
+      </li>
+      <li>
+        Manuel García Ramírez (TU). Suplentes: Ana Ma López Jiménez (TU) y Andrés Santamaría Santigosa (TU).
+      </li>
 
     @elseif ( $step == 3 )
-
-    Manuel l. de la Mata Benítez. Profesor Titular de Universidad. Departamento de Psicología Experimental. Vicedecano de Recursos para el Aprendizaje y la Investigación. Facultad de Psicología.
-    Tfno.: (34) 95 455 62 16. doctorado_psicologia@us.es
-    https://investigacion.us.es/sisius/sis_showpub.php?idpers=1820
+    <h2>Datos Coordinador/a del programa</h2>
+    <p class="lead">&nbsp;</p>
+    <p>Manuel l. de la Mata Benítez. <br>
+    Profesor Titular de Universidad. Departamento de Psicología Experimental. <br>Vicedecano de Recursos para el Aprendizaje y la Investigación. <br>Facultad de Psicología.<br><br>
+    </p>
+   <p>
+    Tfno.: (34) 95 455 62 16. <a href="mailto:doctorado_psicologia@us.es">doctorado_psicologia@us.es</a>
+    </p>
+    <p>
+    <a href="https://investigacion.us.es/sisius/sis_showpub.php?idpers=1820">https://investigacion.us.es/sisius/sis_showpub.php?idpers=1820</a>
+    </p>
 
     @else
-
-    http://www.doctorado.us.es/
-    Servicio de Doctorado - Pabellón de México, Avenida de la Palmera, s/n. 41013 Sevilla http://www.doctorado.us.es/horarios-localizacion
+    <h2>Datos del Servicio de Doctorado</h2>
+    <p class="lead">&nbsp;</p>
+    <p><a href="http://www.doctorado.us.es">http://www.doctorado.us.es/</a>
+    <p>
+    Servicio de Doctorado - Pabellón de México,<br> 
+    Avenida de la Palmera, s/n. 
+    <br>41013 Sevilla 
+    <br><a href="http://www.doctorado.us.es/horarios-localizacion">http://www.doctorado.us.es/horarios-localizacion</a>
+    </p>
     
     @endif
   </div> <!-- /.col -->
